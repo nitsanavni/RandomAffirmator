71164c30f14ac1d8378b624c416371ba693831e8
Nitsan Avni <nitsanav@gmail.com>
2015-05-11 12:22:11 +0300

    add button and shared prefs boolean

--- app/src/main/java/com/randomaffirmator/randomaffirmator/SettingsActivityFragment.java
+++ app/src/main/java/com/randomaffirmator/randomaffirmator/SettingsActivityFragment.java
@@ -1,10 +1,14 @@
 package com.randomaffirmator.randomaffirmator;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -12,12 +16,60 @@ import android.view.ViewGroup;
  */
 public class SettingsActivityFragment extends Fragment {
 
+    private static final String SHARED_PREFS = "RandomAffirmator.SharedPrefs";
+    private static final String KEY_ACTIVE = "SettingsActivityFragment.KEY_ACTIVE";
+    private boolean mActive;
+    private Handler mHandler;
+    private java.lang.Runnable mRunnable;
+
     public SettingsActivityFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SharedPreferences prefs = getActivity()
+                .getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        mActive = prefs.getBoolean(KEY_ACTIVE, false);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        SharedPreferences prefs = getActivity()
+                .getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        prefs.edit().putBoolean(KEY_ACTIVE, mActive).apply();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View ret = inflater.inflate(R.layout.fragment_settings, container, false);
+        final TextView button = (TextView) ret.findViewById(R.id.activate_button);
+        mHandler = new Handler();
+        mRunnable = new Runnable() {
+            @Override
+            public void run() {
+                getActivity().finish();
+            }
+        };
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mActive = !mActive;
+                updateText(button);
+                mHandler.removeCallbacks(mRunnable);
+                mHandler.postDelayed(mRunnable, 1000);
+            }
+        });
+        updateText(button);
+        return ret;
+    }
+
+    private void updateText(TextView button) {
+        if (mActive)
+            button.setText(getString(R.string.deactivate));
+        else
+            button.setText(getString(R.string.activate));
     }
 }
